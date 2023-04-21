0f22f0d431c802d4beaa4af2d31d58445e28f54c
Sizant <superkop76@gmail.com>
2023-04-21 11:23:12 +0530

    'update'

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import LoginForm from "./components/loginForm";
 import CardPage from "./components/card";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
 
 
 function App(){
 
 
     return (
-        <BrowserRouter>
+        <HashRouter>
         <Routes>
             <Route path="/" element={<LoginForm />}/>
             <Route path="/card" element={<CardPage />}/>
         </Routes>
-        </BrowserRouter>
+        </HashRouter>
         
 
     )  
